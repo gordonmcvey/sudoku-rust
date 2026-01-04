d5dcbf6536633920015f31f7658cb253b95b2ef3
gordonmcvey <grj.mcvey@gmail.com>
2026-01-04 22:24:33 +0000

    Re-implemented the grid as a 1 dimensional array Added a method for fetching subgrids (WIP)

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ use sudoku_rust::sudoku::{
 };
 
 fn main() {
-    // let mut grid = Grid::new();
-    //
-    // grid.set_cell(0, 0, 1)
+    let mut grid = Grid::new();
+
+    // grid
+    //     .set_cell(0, 0, 1)
+    //     .set_cell(1, 0, 1)
+    //     .set_cell(2, 0, 1)
     //     .set_cell(1, 1, 2)
     //     .set_cell(2, 2, 3)
     //     .set_cell(3, 3, 4)
@@ -14,19 +17,24 @@ fn main() {
     //     .set_cell(5, 5, 6)
     //     .set_cell(6, 6, 7)
     //     .set_cell(7, 7, 8)
-    //     .set_cell(8, 8, 9);
+    //     .set_cell(8, 8, 9)
+    // ;
 
     let grid = Grid::from_array([
-        [Some(1), None, None, None, None, None, None, None, None],
-        [Some(1), Some(2), None, None, None, None, None, None, None],
-        [Some(1), Some(2), Some(3), None, None, None, None, None, None],
-        [Some(1), Some(2), Some(3), Some(4), None, None, None, None, None],
-        [Some(1), Some(2), Some(3), None, Some(5), None, None, None, None],
-        [Some(1), Some(2), Some(3), None, None, Some(6), None, None, None],
-        [Some(1), Some(2), Some(3), None, None, None, Some(7), None, None],
-        [Some(1), Some(2), Some(3), None, None, None, None, Some(8), None],
-        [Some(1), Some(2), Some(3), None, None, None, None, None, Some(9)],
+        [Some(1), Some(2), Some(3), Some(4), Some(5), Some(6), Some(7), Some(8), Some(9)],
+        [Some(10), None, Some(12), Some(13), None, Some(15), Some(16), None, Some(18)],
+        [Some(19), Some(20), Some(21), Some(22), Some(23), Some(24), Some(25), Some(26), Some(27)],
+        [Some(28), Some(29), Some(30), Some(31), Some(32), Some(33), Some(34), Some(35), Some(36)],
+        [Some(37), None, Some(39), Some(40), None, Some(42), Some(43), None, Some(45)],
+        [Some(46), Some(47), Some(48), Some(49), Some(50), Some(51), Some(52), Some(53), Some(54)],
+        [Some(55), Some(56), Some(57), Some(58), Some(59), Some(60), Some(61), Some(62), Some(63)],
+        [Some(64), None, Some(66), Some(67), None, Some(69), Some(70), None, Some(72)],
+        [Some(73), Some(74), Some(75), Some(76), Some(77), Some(78), Some(79), Some(80), Some(81)],
     ]);
+
+    // for a in grid.grid.chunks(9) {
+    //     println!("{:#?}", a);
+    // }
 
     for row in 0..9 {
         print!("\t");
@@ -46,8 +54,35 @@ fn main() {
         }
     }
 
+    println!("{:?}", grid.row(0));
+    println!("{:?}", grid.row(1));
+    println!("{:?}", grid.row(2));
     println!("{:?}", grid.row(3));
+    println!("{:?}", grid.row(4));
+    println!("{:?}", grid.row(5));
+    println!("{:?}", grid.row(6));
+    println!("{:?}", grid.row(7));
+    println!("{:?}", grid.row(8));
+    println!();
+
+    println!("{:?}", grid.col(0));
+    println!("{:?}", grid.col(1));
+    println!("{:?}", grid.col(2));
     println!("{:?}", grid.col(3));
+    println!("{:?}", grid.col(4));
+    println!("{:?}", grid.col(5));
+    println!("{:?}", grid.col(6));
+    println!("{:?}", grid.col(7));
+    println!("{:?}", grid.col(8));
+    println!("{:?}", grid.col(9));
+    println!();
+
+    println!("{:?}", grid.subgrid(0));
+    println!("{:?}", grid.subgrid(1));
+    println!("{:?}", grid.subgrid(2));
+    println!("{:?}", grid.subgrid(3));
+    println!("{:?}", grid.subgrid(4));
+    println!();
 
     let solver = Solver::new(grid);
     solver.solve();
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -6,7 +6,7 @@ pub struct Game {
 
 #[derive(Debug)]
 pub struct Grid {
-    grid: Vec<Vec<Option<u8>>>,
+    pub grid: [Option<u8>; Grid::MAX_ROWS * Grid::MAX_COLS],
 }
 
 impl Grid {
@@ -15,45 +15,90 @@ impl Grid {
 
     pub fn new() -> Self {
         Self {
-            //grid: [[None; Self::MAX_COLS]; Self::MAX_ROWS],
-            grid: vec![vec![None; Self::MAX_COLS]; Self::MAX_ROWS],
+            grid: [None; Self::MAX_COLS * Self::MAX_COLS],
         }
     }
 
     pub fn from_array(array_grid: [[Option<u8>;9];9]) -> Self {
-        let mut grid = Vec::new();
+        let mut grid = [None; Self::MAX_COLS * Self::MAX_COLS];
+        let mut i = 0;
+
         for row in array_grid.iter() {
-            grid.push(row.to_vec());
+            for cell in row.iter() {
+                grid[i] = *cell;
+                i += 1;
+            }
         }
 
         Self { grid }
     }
 
-    pub fn cell(&self, x: usize, y: usize) -> &Option<u8> {
+    pub fn cell(&self, row_id: usize, col_id: usize) -> &Option<u8> {
         // @todo Range check here
-        &self.grid[x][y]
+        &self.grid[row_id * Self::MAX_ROWS + col_id]
     }
 
-    pub fn row(&self, row_index: usize) -> &[Option<u8>] {
+    pub fn row(&self, row_id: usize) -> &[Option<u8>] {
         // @todo Range check here
-        &self.grid[row_index]
+        &self.grid[row_id * Self::MAX_ROWS..(row_id + 1) * Self::MAX_ROWS]
     }
 
-    pub fn col(&self, col_index: usize) -> Vec<Option<u8>> {
-
-        let mut a:Vec<Option<u8>> = Vec::new();
-
-        for row in self.grid.iter() {
-            a.push(row[col_index]);
-        }
-        a
+    // @todo This is probably not the preferred way to extrapolate the columns and it returns a Vec
+    // instead of an array slice
+    pub fn col(&self, col_id: usize) -> Vec<&Option<u8>> {
+        // @todo Range check here
+        /*
+         * As we're simulating the grid with a 1-dimensional array, extracting a "column" involves
+         * fetching every nth element from the array where n is the width of the grid, and offsetting
+         * by a column ID in the range 0 .. n - 1 such that (for a grid that's 9 elements wide)
+         * column 0 equates to elements [0, 9, 18 ...], column 1 is [1, 10, 19 ...], column 3 is
+         * [2, 11, 20 ...] and so on
+         */
+        self.grid
+            .iter()
+            .skip(col_id)
+            .step_by(Self::MAX_COLS)
+            .collect::<Vec<&Option<u8>>>()
     }
 
-    // @todo Implement getters for subgrids
-
-    pub fn set_cell(&mut self, x: usize, y: usize, value: u8) -> &mut Self {
+    // @todo This is a pretty hacky POC and could use a refactor into something that handles
+    // selecting the subslices more elegantly and it should return a Vec instead of an array slice
+    pub fn subgrid(&self, subgrid_id: usize) -> Vec<Option<u8>> {
         // @todo Range check here
-        self.grid[x][y] = Some(value);
+        /*
+         * As we're simulating the grid with a 1-dimensional array, a "subgrid" can be considered to
+         * be 3 sub-slices of 3 elements each, comprising a total of the 9 elements that make up the
+         * subgrid.
+         *
+         * The start of the first slice of each sub grid lies in column 0, 3, or 6, and row 0, 3, or
+         * 6.  If we're using IDs of 0 .. 8 to refer to each subgrid, then that means that the first
+         * slice of each subgrid starts at index 0, 3, 6, then skips to 27, 30, 33, and so on.  Each
+         * slice is 3 elements long, and the entire subgrid is made up of the sub-slices starting in
+         * the same column from the next two rows (so subgrid 0 consists of the array elements
+         * [0, 1, 2, 9, 10, 11, 18, 19, 20], subgrid 4 would consist of the elements
+         * [30, 31, 32, 39, 40, 41, 48, 49, 50], and so on)
+         */
+        let subgrid_col = subgrid_id * 3 % 9;
+        let subgrid_row = ((subgrid_id * 9) / 27) * 27;
+        let subgrid_index = subgrid_col + subgrid_row;
+
+        // println!("Row: {}, Col: {}, Index: {}", subgrid_row, subgrid_col, subgrid_index);
+
+        // println!("subgrid row 1 {:?}:", &self.grid[subgrid_index .. subgrid_index + 3]);
+        // println!("subgrid row 1 {:?}:", &self.grid[subgrid_index + 9 .. subgrid_index + 3 + 9]);
+        // println!("subgrid row 1 {:?}:", &self.grid[subgrid_index + 18 .. subgrid_index + 3 + 18]);
+
+        let mut subgrid:Vec<Option<u8>> = Vec::new();
+        subgrid.extend_from_slice(&self.grid[subgrid_index .. subgrid_index + 3]);
+        subgrid.extend_from_slice(&self.grid[subgrid_index + 9 .. subgrid_index + 3 + 9]);
+        subgrid.extend_from_slice(&self.grid[subgrid_index + 18 .. subgrid_index + 3 + 18]);
+
+        subgrid
+    }
+
+    pub fn set_cell(&mut self, row_id: usize, col_id: usize, value: u8) -> &mut Self {
+        // @todo Range check here
+        self.grid[row_id * Self::MAX_ROWS + col_id] = Some(value);
         self
     }
 }
@@ -88,7 +133,7 @@ impl Solver {
         } else if column_id > 8 {
             // If we've passed the end of this row then move to the next one
             return self.find_solution(row_id + 1, 0);
-        } else if self.problem.grid[row_id][column_id].is_some() {
+        } else if self.problem.cell(row_id, column_id).is_some() {
             // If this cell already has a value, move on to the next one
             println!("[{}, {}] is already filled", row_id, column_id);
             return self.find_solution(row_id, column_id + 1);
