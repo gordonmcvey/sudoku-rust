044dcd331e68aa2c486b34724b5e810896c68d7d
gordonmcvey <grj.mcvey@gmail.com>
2026-01-12 18:24:34 +0000

    Fixed transcription error in the test puzzle

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         [Some(4), None, None, Some(8), None, Some(3), None, None, Some(1)],
         [Some(7), None, None, None, Some(2), None, None, None, Some(6)],
 
-        [None, Some(6), None, None, None, None, Some(2), Some(0), None],
+        [None, Some(6), None, None, None, None, Some(2), Some(8), None],
         [None, None, None, Some(4), Some(1), Some(9), None, None, Some(5)],
         [None, None, None, None, Some(8), None, None, Some(7), Some(9)],
     ]);
