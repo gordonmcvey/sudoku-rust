b173e48000ac1ee00fa0e4d05be28ed536ee5692
gordonmcvey <grj.mcvey@gmail.com>
2026-01-16 12:34:26 +0000

    Added an example of an unsolvable puzzle

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,26 @@ fn main() {
         [None, None, None, None, Some(8), None, None, Some(7), Some(9)],
     ]).unwrap();
 
+    // Modified version of the above puzzle to have a valid initial state but also be unsolvable
+    // let grid = Grid::from_array([
+    //     [Some(5), Some(3), None, None, Some(4), None, None, None, None],
+    //     [Some(6), None, None, Some(1), Some(9), Some(5), None, None, None],
+    //     [None, Some(9), Some(8), None, None, None, None, Some(6), None],
+    //
+    //     [Some(8), None, None, None, Some(6), None, None, None, Some(3)],
+    //     [Some(4), None, None, Some(8), None, Some(3), None, None, Some(1)],
+    //     [Some(7), None, None, None, Some(2), None, None, None, Some(6)],
+    //
+    //     [None, Some(6), None, None, None, None, Some(2), Some(8), None],
+    //     [None, None, None, Some(4), Some(1), Some(9), None, None, Some(5)],
+    //     [None, None, None, None, Some(8), None, None, Some(7), Some(9)],
+    // ]).unwrap();
+
     // Puzzle from https://en.wikipedia.org/wiki/Sudoku_solving_algorithms
     //
     // NOTE: This puzzle is designed to be pathological for machine solving algorithms.  It does have
     // a solution, but it will take a while for a simple solver to find it, especially for the debug
     // build
-
     // let grid = Grid::from_array([
     //     [None, None, None, None, None, None, None, None, None],
     //     [None, None, None, None, None, Some(3), None, Some(8), Some(5)],
